9192b3f5bd434f783f3b59e77ed95b6c4f582311
Ovidiu Cota <ovidiucota@yahoo.com>
2018-06-04 14:36:27 +0300

    Updates solution to prime-factors

--- prime-factors/prime-factors.js
+++ prime-factors/prime-factors.js
@@ -1,32 +1,17 @@
-let primeFactors = function () {}
-
-primeFactors.for = function (number) {
-  let result = []
-
-  let i = 2
-  while (i <= number) {
-    if (number % i === 0) {
-      result.push(i)
-      number /= i
-    } else {
-      i = nextPrime(i)
+const primeFactors = {
+  for: function (number) {
+    let result = []
+    let factor = 2
+    while (number >= factor) {
+      if (number % factor === 0) {
+        result = [...result, factor]
+        number /= factor
+      } else {
+        factor += 1
+      }
     }
+    return result
   }
-  return result
-}
-
-let isPrime = function (num) {
-  if (num <= 1) return false
-  for (let i = 2; i < Math.sqrt(num); i++) {
-    if (num % i === 0) return false
-  }
-  return true
-}
-
-let nextPrime = function (number) {
-  let i = number + 1
-  while (!isPrime(i)) i++
-  return i
 }
 
 module.exports = primeFactors
